a6205a166abf532e32ad7d7bdc12c0d445fd83d0
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-04-16 20:54:42 +0200

    Add files via upload

--- website/js/bar_charts.js
+++ website/js/bar_charts.js
@@ -1,50 +1,67 @@
-// Data to be displayed
-const data = [10, 20, 30, 40, 50];
+{ // scoping var in the file 
 
-// Set up dimensions for the chart
-const width = 500;
-const height = 300;
-const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+csvUrl = "https://raw.githubusercontent.com/kirbs-btw/ufo-data-analysis/main/data/clean_ufo_data.csv";
 
-// Create SVG element
-const svg = d3.select("#bar-chart-test")
-    .attr("width", width)
-    .attr("height", height);
+d3.csv(csvUrl).then(function(data) {
+    // counting the num of entrys per year
+    let yearCounts = {};
+    data.forEach(function(d) {
+      const year = +d.year;
+      if (yearCounts[year]) {
+        yearCounts[year]++;
+      } else {
+        yearCounts[year] = 1;
+      }
+    });
 
-// Create scales for x and y axis
-const x = d3.scaleBand()
-    .domain(data.map((d, i) => i)) // Assuming data is an array, use index for x-axis
-    .range([margin.left, innerWidth + margin.left])
+    // Chart dimensions
+    var width = 600;
+    var height = 400;
+    var margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+    // SVG element
+    var svg = d3.select("#bar-chart-test")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // X scale
+    var x = d3.scaleBand()
+    .domain(Object.keys(yearCounts))
+    .range([0, width])
     .padding(0.1);
 
-const y = d3.scaleLinear()
-    .domain([0, d3.max(data)])
+    // Y scale
+    var y = d3.scaleLinear()
+    .domain([0, d3.max(Object.values(yearCounts))])
     .nice()
-    .range([innerHeight + margin.top, margin.top]);
+    .range([height, 0]);
 
-// Create x axis
-const xAxis = d3.axisBottom(x);
-
-// Create y axis
-const yAxis = d3.axisLeft(y);
-
-// Append group elements for axes
-svg.append("g")
-    .attr("transform", `translate(0, ${innerHeight + margin.top})`)
-    .call(xAxis);
-
-svg.append("g")
-    .attr("transform", `translate(${margin.left}, 0)`)
-    .call(yAxis);
-
-// Create bars
-svg.selectAll("rect")
-    .data(data)
-    .join("rect")
+    // Bars
+    svg.selectAll(".bar")
+    .data(Object.entries(yearCounts))
+    .enter().append("rect")
     .attr("class", "bar")
-    .attr("x", (d, i) => x(i))
-    .attr("y", d => y(d))
+    .attr("x", d => x(d[0]))
     .attr("width", x.bandwidth())
-    .attr("height", d => innerHeight + margin.top - y(d));
+    .attr("y", d => y(d[1]))
+    .attr("height", d => height - y(d[1]));
+
+    // X axis
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    // Y axis
+    svg.append("g")
+    .call(d3.axisLeft(y));
+
+
+})
+.catch(function(error) {
+  console.error("Error loading the CSV file:", error);
+});
+
+} // scoping
